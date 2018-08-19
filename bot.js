4b00375283916c0cc6a54f092e919eebaeec3323
jok999 <42048536+jok999@users.noreply.github.com>
2018-08-20 00:29:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,6 +31,22 @@ client.on('ready', () => {
       }
   });
 
+        client.on('message', message => {
+          var prefix = '%';
+          if (message.content.startsWith(prefix + 'موسيقى')) {
+      let embed = new Discord.RichEmbed()
+      .addField('%play' ,'   :headphones:تشغيل الاغنيه باسم او برابط:headphones: ')
+      .addField('%stop' ,'    :mute: ايقاف الاغنيه:mute:   ')
+      .addField('%skip' ,'     :play_pause: تخطي الاغنيه :play_pause:  ')
+      .addField('%pause' ,'    :clock1: ايقاف الاغنيه مؤقت:clock1:  ')
+      .addField('%resume' ,'    :musical_note: تكملةالاغنيه:musical_note:  ')
+      .addField('%queue' ,'    :pencil: اظهار قائمة التشغيل :pencil:   ')
+      .addField('%np' ,'    :page_facing_up: اظهار الاغنية اللي انت مشغلها حاليا:page_facing_up:  ')
+      .setColor('RANDOM')
+      .setTitle('═════ஜ۩۞۩ஜ══════════ஜ۩۞۩ஜ═════')
+      message.channel.send(embed)
+      }
+  });
 
 
 
