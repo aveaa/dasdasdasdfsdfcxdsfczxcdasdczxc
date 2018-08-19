575ada39ef2c97a722469751c0fa13f88c25adea
jok999 <42048536+jok999@users.noreply.github.com>
2018-08-20 00:41:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,136 @@ client.on('ready', () => {
 });
 
 
+client.on('message', message => {
+    if (message.content.startsWith("%tg")) {
+      
+        const translate = require('google-translate-api');
+        const Discord = require('discord.js');
+
+    let toTrans = message.content.split(' ').slice(1);
+    let language;
+
+    language = toTrans[toTrans.length - 2] === 'to' ? toTrans.slice(toTrans.length - 2, toTrans.length)[1].trim() : undefined;
+    if (!language) {
+        return message.reply(`Please supply valid agruments.\n**Example** \`%tg text to language\``);
+    }
+    let finalToTrans = toTrans.slice(toTrans.length - toTrans.length, toTrans.length - 2).join(' ');
+    translate(finalToTrans, {to: language}).then(res => {
+            message.channel.send({embed: {
+                color: 3447003,
+                author: {
+                  name: 'Dragon\'s translator',
+                  icon_url: client.user.avatarURL
+                },
+                fields: [{
+                    name: "Translator",
+                    value: `**From:** ${res.from.language.iso}\n\`\`\`${finalToTrans}\`\`\`\n**To: **${language}\n\`\`\`${res.text}\`\`\``
+                  }
+                ],
+                timestamp: new Date(),
+                footer: {
+                  icon_url: client.user.avatarURL,
+                  text: "الترجمه"
+                }
+              }
+            });
+    }).catch(err => {
+        message.channel.send({
+            embed: {
+                description: '❌ We could not find the supplied language.',
+                color: 0xE8642B
+              
+            }
+        });
+    });
+    }
+});
+
+client.on("message", message => {
+    var prefix = "%"
+    if (!message.content.startsWith(prefix)) return;
+      let command = message.content.split(" ")[0];
+      command = command.slice(prefix.length);
+        if(command === "min") {
+                const args = message.content.split(" ").slice(1).join(" ")
+        if (!args) return message.channel.send("** Type your skin name **");
+        const image = new Discord.Attachment(`https://minotar.net/armor/body/${args}`, "skin.png");
+    message.channel.send(image)
+        }
+    });
+
+ client.on('message', message => {
+    var prefix = "%"
+if (message.content.startsWith(prefix + "time")) {
+   let uptime = client.uptime;
+
+   let days = 0;
+   let hours = 0;
+   let minutes = 0;
+   let seconds = 0;
+   let notCompleted = true;
+
+   while (notCompleted) {
+
+       if (uptime >= 8.64e+7) {
+
+           days++;
+           uptime -= 8.64e+7;
+
+       } else if (uptime >= 3.6e+6) {
+
+           hours++;
+           uptime -= 3.6e+6;
+
+       } else if (uptime >= 60000) {
+
+           minutes++;
+           uptime -= 60000;
+
+       } else if (uptime >= 1000) {
+           seconds++;
+           uptime -= 1000;
+
+       }
+
+       if (uptime < 1000)  notCompleted = false;
+
+   }
+
+   message.channel.send("`" + `${days} days, ${hours} hrs, ${minutes} min , ${seconds} sec` + "`");
+
+
+
+client.on('message', message => {
+  if (message.content.startsWith("%صوره")) {
+      var mentionned = message.mentions.users.first();
+  var Nizx;
+    if(mentionned){
+        var Nizx = mentionned;
+    } else {
+        var Nizx = message.author;
+        
+    }
+      const embed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setImage(`${Nizx.avatarURL}`)
+    message.channel.sendEmbed(embed);
+  }
+});
+
+client.on('message', message => {
+  if (message.content === "%bo") {
+         if(!message.channel.guild) return message.reply('** This command only for servers **');
+  let embed = new Discord.RichEmbed()
+.setColor('RANDOM')
+.addField("**اسم السيرفر**", message.guild.name)
+.addField("**عدد السيرفرات الي فيها البوت:**" , client.guilds.size)
+.addField("**المستخدمين:**", client.users.size)
+.addField("**قنوات:**", client.channels.size)
+message.channel.sendEmbed(embed);
+ }
+});
+
         client.on('message', message => {
           var prefix = '%';
           if (message.content.startsWith(prefix + 'جديد')) {
@@ -31,6 +161,7 @@ client.on('ready', () => {
       }
   });
 
+
         client.on('message', message => {
           var prefix = '%';
           if (message.content.startsWith(prefix + 'موسيقى')) {
@@ -47,6 +178,29 @@ client.on('ready', () => {
       message.channel.send(embed)
       }
   });
+
+
+
+        client.on('ready', function(){
+          var ms = 10000 ;
+          var setGame = ['%help','%دعوه','%help','%دعوه','%help'];
+          var i = -1;
+          var j = 0;
+          setInterval(function (){
+              if( i == -1 ){
+                  j = 1;
+              }
+              if( i == (setGame.length)-1 ){
+                  j = -1;
+              }
+              i = i+j;
+              client.user.setGame(setGame[i],`https://www.twitch.tv/jokar_999`);
+          }, ms);
+      
+      });
+
+
+ 
 
         client.on('message', message => {
           var prefix = '%';
@@ -87,7 +241,57 @@ client.on('ready', () => {
       }
   });
 
+  client.on('message', msg => {
+    var prefix = "%"
+  if(msg.content.startsWith (prefix  + 'server')) {
+  let embed = new Discord.RichEmbed()
+  .setColor('RANDOM')
+  .setThumbnail(msg.guild.iconURL)
+  .setTitle(`Showing Details Of  **${msg.guild.name}**`)
+  .addField(':globe_with_meridians: نوع السيرفر', `[** ${msg.guild.region}**]`,true)
+  .addField(':medal: الرتب',`[** __${msg.guild.roles.size}__ **]`,true)
+  .addField(':red_circle: عدد الاعضاء',`[** __${msg.guild.memberCount}__ **]`,true)
+  .addField(':large_blue_circle: عدد الاعضاء الاونلاين',`[** __${msg.guild.members.filter(m=>m.presence.status == 'online').size}__ **]`,true)
+  .addField(':pencil: الرومات الكتابية',`[** __${msg.guild.channels.filter(m => m.type === 'text').size}__** ]`,true)
+  .addField(':microphone: رومات الصوت',`[** __${msg.guild.channels.filter(m => m.type === 'voice').size}__ **]`,true)
+  .addField(':crown: الأونـر',`**${msg.guild.owner}**`,true)
+  .addField(':id: ايدي السيرفر',`**${msg.guild.id}**`,true)
+  .addField(':date: تم عمل السيرفر في',msg.guild.createdAt.toLocaleString())
+  msg.channel.send(embed);
+  }
+});
 
+ client.on('message', message => {
+  if(!message.channel.guild) return;
+if (message.content.startsWith('%ping')) {
+if(!message.channel.guild) return;
+var msg = `${Date.now() - message.createdTimestamp}`
+var api = `${Math.round(client.ping)}`
+if (message.author.bot) return;
+let embed = new Discord.RichEmbed()
+.setAuthor(message.author.username,message.author.avatarURL)
+.setColor('RANDOM')
+.addField('**Time Taken:**',msg + " ms 📶 ")
+.addField('**WebSocket:**',api + " ms 📶 ")
+message.channel.send({embed:embed});
+}
+});
+
+        client.on('message', message => {
+          var prefix = '%';
+          if (message.content.startsWith(prefix + 'العاب')) {
+      let embed = new Discord.RichEmbed()
+      .addField('%عقاب' ,'لعبة العقاب')
+      .addField('%حكم' ,'لعبة الحكم')
+      .addField('%مريم' ,'لعبة مريم')
+      .addField('%صراحه' ,'لعبة الصراحه')
+      .addField('%خواطر' ,'لعبة خواطر')
+      .addField('%اسئلني' ,'لعبه اسئلني يعطيك سؤال يجيب عليك الجواب عليه خلال دقيقه')
+      .setColor('RANDOM')
+      .setTitle('═════ஜ۩۞۩ஜ══════════ஜ۩۞۩ஜ═════')
+      message.channel.send(embed)
+      }
+  });
 
 
 
