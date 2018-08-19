ca173fa0f8d439d6d92273bf3e2e7860fc0b4af0
jok999 <42048536+jok999@users.noreply.github.com>
2018-08-19 14:03:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -105,7 +105,18 @@ if (message.content.startsWith(prefix + "time")) {
 
    message.channel.send("`" + `${days} days, ${hours} hrs, ${minutes} min , ${seconds} sec` + "`");
 
-
+client.on("message", message => {
+    var prefix = "%"
+    if (!message.content.startsWith(prefix)) return;
+      let command = message.content.split(" ")[0];
+      command = command.slice(prefix.length);
+        if(command === "min") {
+                const args = message.content.split(" ").slice(1).join(" ")
+        if (!args) return message.channel.send("** Type your skin name **");
+        const image = new Discord.Attachment(`https://minotar.net/armor/body/${args}`, "skin.png");
+    message.channel.send(image)
+        }
+    });
 
 client.on('message', message => {
   if (message.content.startsWith("%صوره")) {
