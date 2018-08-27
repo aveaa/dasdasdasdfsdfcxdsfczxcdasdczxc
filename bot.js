6e5d367b9826da3b075fbc7232df32b69d815e78
jok999 <42048536+jok999@users.noreply.github.com>
2018-08-28 02:37:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,17 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-
+  client.on('message', message => {
+    if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('%adminsbc')){
+if(!message.author.id === 'bot_owner_id') return;
+message.channel.sendMessage('**جار ارسال الرسالة |✅**')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
 
 
 
