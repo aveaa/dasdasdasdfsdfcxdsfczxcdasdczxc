94e589aeb97c5275849747b7e4a35b1a061df94b
jok999 <42048536+jok999@users.noreply.github.com>
2018-08-28 02:04:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,29 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
- 
+ const devs = ['292042690470739968' , '310129588397277185' , '' , ''];
+const adminprefix = "%";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  }
+  });
 
  client.on('message' , message => {
   var prefix = "%";
