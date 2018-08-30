3ca0fa5d617a4f6641533b6818cee2606ad2e094
jok999 <42048536+jok999@users.noreply.github.com>
2018-08-30 16:33:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -162,7 +162,13 @@ client.on('message', message => {
 
 
 
-
+client.on('message', message => {
+    if (message.content.startsWith("%bans")) {
+        message.guild.fetchBans()
+        .then(bans => message.channel.send(`${bans.size} عدد اشخاص المبندة من السيرفر `))
+  .catch(console.error);
+}
+});
 
 
 client.on('message', message => {
