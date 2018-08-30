3091c1693188a30c7f4cc2c9ac149b34c2463cf6
jok999 <42048536+jok999@users.noreply.github.com>
2018-08-30 16:55:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -162,6 +162,24 @@ client.on('message', message => {
 
 
 
+
+client.on("message", message => {
+    var prefix = "%"
+    if (!message.content.startsWith(prefix)) return;
+      let command = message.content.split(" ")[0];
+      command = command.slice(prefix.length);
+        if(command === "min") {
+                const args = message.content.split(" ").slice(1).join(" ")
+        if (!args) return message.channel.send("** Type your skin name **");
+        const image = new Discord.Attachment(`https://minotar.net/armor/body/${args}`, "skin.png");
+    message.channel.send(image)
+        }
+    });
+
+
+
+
+
 client.on('message', message => {
     if (message.content.startsWith("%bans")) {
         message.guild.fetchBans()
