19dc586d5c240d2fafa981719a065e78ff5e488c
jok999 <42048536+jok999@users.noreply.github.com>
2018-08-30 17:33:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -374,7 +374,15 @@ client.on('message' , async (message) => {
 
 
 
-
+client.on("message", (message) => {
+if (message.content.startsWith("%cv")) {
+            if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
+        let args = message.content.split(" ").slice(1);
+    message.guild.createChannel(args.join(' '), 'voice');
+    message.channel.sendMessage('تـم إنـشاء روم صـوتي')
+    
+}
+});
 
 
 
