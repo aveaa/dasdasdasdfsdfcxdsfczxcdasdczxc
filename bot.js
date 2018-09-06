838a99802e510aced3c24f1c4dba03b7dc2f2433
jok999 <42048536+jok999@users.noreply.github.com>
2018-09-06 15:45:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -355,19 +355,58 @@ http://cutt.us/CJ_Bot
 
 
 
-    client.on('guildMemberAdd', member => {
-      var embed = new Discord.RichEmbed()
-      .setAuthor(member.user.username, member.user.avatarURL)
-      .setThumbnail(member.user.avatarURL)
-      .setTitle(`عضو جديد`)
-      .setDescription(`اهلا بك في السيرفر`)
-      .addField(' :bust_in_silhouette:  انت رقم',`**[ ${member.guild.memberCount} ]**`,true)
-      .setColor('GREEN')
-  
-  var channel =member.guild.channels.find('name', 'welcome')
-  if (!channel) return;
-  channel.send({embed : embed});
-  });
+client.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', 'welcome');
+    let memberavatar = member.user.avatarURL
+      if (!channel) return;
+    let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(memberavatar)
+        .addField(':running_shirt_with_sash: | name :  ',`${member}`)
+        .addField(':loudspeaker: | نورت السيرفر يا قلبي' , `Welcome to the server, ${member}`)
+        .addField(':id: | user :', "**[" + `${member.id}` + "]**" )
+                .addField('➡| انت العضو رقم',`${member.guild.memberCount}`)
+               
+                  .addField("Name:",`<@` + `${member.id}` + `>`, true)
+                     
+                                     .addField(' الـسيرفر', `${member.guild.name}`,true)
+                                       
+     .setFooter(`${member.guild.name}`)
+        .setTimestamp()
+   
+      channel.sendEmbed(embed);
+    });
+
+
+
+
+
+
+client.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', 'ألتہرحہيہبہ');
+    let memberavatar = member.user.avatarURL
+      if (!channel) return;
+    let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(memberavatar)
+        .addField(':running_shirt_with_sash: | name :  ',`${member}`)
+        .addField(':loudspeaker: | نورت السيرفر يا قلبي' , `Welcome to the server, ${member}`)
+        .addField(':id: | user :', "**[" + `${member.id}` + "]**" )
+                .addField('➡| انت العضو رقم',`${member.guild.memberCount}`)
+               
+                  .addField("Name:",`<@` + `${member.id}` + `>`, true)
+                     
+                                     .addField(' الـسيرفر', `${member.guild.name}`,true)
+                                       
+     .setFooter(`${member.guild.name}`)
+        .setTimestamp()
+   
+      channel.sendEmbed(embed);
+    });
+
+
+
+
 
 
     client.on('guildMemberAdd', member => {
