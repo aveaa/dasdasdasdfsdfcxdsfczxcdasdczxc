bbc852758a03261d697644ec99c8cfef7d433e8f
jok999 <42048536+jok999@users.noreply.github.com>
2018-09-06 16:12:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1356,24 +1356,7 @@ var fkk =[
 
 
 
-var prefix = "$"
 
-client.on('message' , async (message) => {
- if (message.content.startsWith(prefix + 'w')) {
-  const args = message.content.substring(prefix.length).split(' ');
-
- message.delete();
-args.shift() 
-let msg = args.join(' ') 
-message.channel.createWebhook(message.author.username, message.author.avatarURL) 
-    .then(wb => {
-        const user = new Discord.WebhookClient(wb.id, wb.token) 
-        user.send(msg); 
-        user.delete() 
-    })
-    .catch(console.error)
- }
-});
 
 
 
